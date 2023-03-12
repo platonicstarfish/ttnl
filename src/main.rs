f6cc7834c205e39f1ee716dd5e65f01461dadfdd
platonicstarfish <platonicstarfish@protonmail.com>
2023-03-12 23:43:52 +0100

    Initial cli parameters with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{Parser};
+
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short = 'b', default_value = "::1")]
+    bind_addr: std::net::IpAddr,
+    
+    #[arg(short = 'p')]
+    bind_port: u16,
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("{0} : {1}", cli.bind_addr, cli.bind_port);
 }
